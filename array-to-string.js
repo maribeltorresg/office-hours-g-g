795f7923705c57a6404a25cfdcb91d7a45b0503a
maribeltorresg <maribel.torres.guevara@gmail.com>
2017-10-04 22:42:46 -0500

    solucion del ejercicio

--- array-to-string.js
+++ array-to-string.js
@@ -3,3 +3,11 @@ Crear una funcion que reciba como parametro un array y retorne una cadena
 -> retorne quack! sneeze! boom!
 */
 var arr = ["quack","sneeze","boom"];
+
+function convertToString(array){
+  var string = array.join('! ') + '!';
+  return string;
+}
+
+var result = convertToString(arr);
+console.log(result);
